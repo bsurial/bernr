703cdd0e9437414952cbfb4e7b26ce1b5a14f598
bsurial <surial.bernard@gmail.com>
2019-09-13 10:06:48 +0200

    fixes bug that takes new col_name

--- R/print_confint.R
+++ R/print_confint.R
@@ -17,19 +17,19 @@
 #' @examples
 #' library(dplyr)
 #' mtcars %>%
-#' summarise(mean = mean(mpg),
-#'           n = n(),
-#'           se = sd(mpg)/sqrt(n),
-#'           lcl = mean - 1.96 * se,
-#'           ucl = mean + 1.96 * se) %>%
-#'           print_confint("mean_ci", estimate = mean, lcl = lcl,
-#'                       ucl = ucl, digits = 2, to = " to ")
+#'   summarise(mean = mean(mpg),
+#'             n = n(),
+#'             se = sd(mpg)/sqrt(n),
+#'             lcl = mean - 1.96 * se,
+#'             ucl = mean + 1.96 * se) %>%
+#'             print_confint("mean_ci", estimate = mean, lcl = lcl,
+#'                           ucl = ucl, digits = 2, to = " to ")
 print_confint <- function(df, new_col, estimate, lcl, ucl, digits = 1, to = " - ") {
   estimate <- dplyr::enquo(estimate)
   lcl <- dplyr::enquo(lcl)
   ucl <- dplyr::enquo(ucl)
   df %>%
-    dplyr::mutate(new_col := paste0(round(!!estimate, digits), " (95% CI ",
-                                    round(!!lcl, digits), to,
-                                    round(!!ucl, digits), ")"))
+    dplyr::mutate(!!new_col := paste0(round(!!estimate, digits), " (95% CI ",
+                                      round(!!lcl, digits), to,
+                                      round(!!ucl, digits), ")"))
 }
