a8ebc618e37c3ef01ef9de149a5e513e322cd82a
Bernard Surial <surial.bernard@gmail.com>
2020-06-08 11:41:31 +0200

    added functions

---
+++ R/nice_p.R
@@ -0,0 +1,24 @@
+#' Create nice P-values
+#'
+#' This function returns nice p-values. This is useful especially for us with rmarkdown files and knitr::tables.
+#'
+#' @param pval P-value to be converted
+#' @param digits Number of digits to round
+#'
+#' @return A numeric object
+#' @export
+#'
+#' @examples
+#' set.seed(1)
+#' x <- rnorm(100)
+#' y <- rnorm(100)
+#'
+#' t <- t.test(x, y)
+#' nice_p(t$p.value)
+#'
+nice_p <- function(pval, digits = 4) {
+  if(!is.numeric(pval)) {
+    stop("pval has tu be numeric")
+  }
+  ifelse(pval < 0.001, "<0.001", format(round(pval, digits), scientific = FALSE))
+}
