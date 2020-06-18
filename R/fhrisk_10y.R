eaf63874ba72c4bfb157a3244d3017ba8a3003e5
Bernard Surial <surial.bernard@gmail.com>
2020-06-18 08:58:38 +0200

    formula caclulating FH risk

--- R/fhrisk_10y.R
+++ R/fhrisk_10y.R
@@ -1,12 +1,12 @@
 
 #' Framingham Risk Calculator (10 Year Cardiovascular risk)
 #'
-#' This functions calculates the 10-year cardiovascular risk based on the Framingham Risk Score. I used data from 
+#' This functions calculates the 10-year cardiovascular risk based on the Framingham Risk Score. I used data from
 #' \href{https://en.wikipedia.org/wiki/Framingham_Risk_Score}{Wikipedia}.
 #'
 #' @param gender "male" or "female"
 #' @param age numeric
-#' @param cholesterol numeric, default is mg/dL 
+#' @param cholesterol numeric, default is mg/dL
 #' @param smoke logical whether is smoking or not
 #' @param hdl numeric, default is mg/dL
 #' @param sysbp numeric, systolic blood pressure
@@ -17,10 +17,12 @@
 #' @export
 #'
 #' @examples
-#' fhrisk_10y(gender = "male", age = 60, cholesterol = 7, hdl = 1.2, sysbp = 120, bp_treated = TRUE, mg_dl = FALSE)
-#' 
+#' fhrisk_10y(gender = "male", age = 60, cholesterol = 7,
+#'            hdl = 1.2, sysbp = 120, bp_treated = TRUE,
+#'            mg_dl = FALSE)
+#'
 fhrisk_10y <- function(gender, age, cholesterol, smoke, hdl, sysbp, bp_treated, mg_dl = TRUE) {
-  
+
   if(mg_dl == TRUE) {
     hdl <- hdl
     cholesterol <- cholesterol
@@ -28,9 +30,9 @@ fhrisk_10y <- function(gender, age, cholesterol, smoke, hdl, sysbp, bp_treated, 
     hdl <- 38.941 * hdl
     cholesterol <- 38.941 * cholesterol
   }
-  
+
   # Age ---------------------------------------------------------------------
-  
+
   fhrisk_age <- function(gender, age) {
     points <- 0
     if(gender == "female"){
@@ -57,7 +59,7 @@ fhrisk_10y <- function(gender, age, cholesterol, smoke, hdl, sysbp, bp_treated, 
       } else if(age >= 75 & age <80) {
         points <- 16
       }
-      
+
     } else if(gender == "male") {
       if(age <20 | age >= 80) {
         points <- NA
@@ -85,14 +87,14 @@ fhrisk_10y <- function(gender, age, cholesterol, smoke, hdl, sysbp, bp_treated, 
     }
     return(points)
   }
-  
-  
-  
+
+
+
   # Cholesterol -------------------------------------------------------------
-  
+
   fhrisk_chol <- function(gender, age, cholesterol) {
     points <- 0
-    
+
     if(gender == "female"){
       if(age <20 | age >= 80) {
         points <- NA
@@ -108,7 +110,7 @@ fhrisk_10y <- function(gender, age, cholesterol, smoke, hdl, sysbp, bp_treated, 
         } else if(cholesterol >= 280) {
           points <- 13
         }
-        
+
       } else if(age >= 40 & age <50){
         if(cholesterol < 160) {
           points <- 0
@@ -121,7 +123,7 @@ fhrisk_10y <- function(gender, age, cholesterol, smoke, hdl, sysbp, bp_treated, 
         } else if(cholesterol >= 280) {
           points <- 10
         }
-        
+
       } else if(age >= 50 & age <60){
         if(cholesterol < 160) {
           points <- 0
@@ -134,7 +136,7 @@ fhrisk_10y <- function(gender, age, cholesterol, smoke, hdl, sysbp, bp_treated, 
         } else if(cholesterol >= 280) {
           points <- 7
         }
-        
+
       } else if(age >= 60 & age <70) {
         if(cholesterol < 160) {
           points <- 0
@@ -147,7 +149,7 @@ fhrisk_10y <- function(gender, age, cholesterol, smoke, hdl, sysbp, bp_treated, 
         } else if(cholesterol >= 280) {
           points <- 4
         }
-        
+
       } else if(age >= 70 & age <80) {
         if(cholesterol < 160) {
           points <- 0
@@ -161,8 +163,8 @@ fhrisk_10y <- function(gender, age, cholesterol, smoke, hdl, sysbp, bp_treated, 
           points <- 2
         }
       }
-      
-      
+
+
     } else if(gender == "male") {
       if(age <20 | age >= 80) {
         points <- NA
@@ -178,7 +180,7 @@ fhrisk_10y <- function(gender, age, cholesterol, smoke, hdl, sysbp, bp_treated, 
         } else if(cholesterol >= 280) {
           points <- 11
         }
-        
+
       } else if(age >= 40 & age <50){
         if(cholesterol < 160) {
           points <- 0
@@ -191,7 +193,7 @@ fhrisk_10y <- function(gender, age, cholesterol, smoke, hdl, sysbp, bp_treated, 
         } else if(cholesterol >= 280) {
           points <- 8
         }
-        
+
       } else if(age >= 50 & age <60){
         if(cholesterol < 160) {
           points <- 0
@@ -204,7 +206,7 @@ fhrisk_10y <- function(gender, age, cholesterol, smoke, hdl, sysbp, bp_treated, 
         } else if(cholesterol >= 280) {
           points <- 5
         }
-        
+
       } else if(age >= 60 & age <70) {
         if(cholesterol < 160) {
           points <- 0
@@ -217,7 +219,7 @@ fhrisk_10y <- function(gender, age, cholesterol, smoke, hdl, sysbp, bp_treated, 
         } else if(cholesterol >= 280) {
           points <- 3
         }
-        
+
       } else if(age >= 70 & age <80) {
         if(cholesterol < 160) {
           points <- 0
@@ -234,9 +236,9 @@ fhrisk_10y <- function(gender, age, cholesterol, smoke, hdl, sysbp, bp_treated, 
     }
     return(points)
   }
-  
-  
-  
+
+
+
   # Smoke -------------------------------------------------------------------
   fhrisk_smoke <- function(gender, age, smoke) {
     points <- 0
@@ -254,7 +256,7 @@ fhrisk_10y <- function(gender, age, cholesterol, smoke, hdl, sysbp, bp_treated, 
       } else if(age >= 70 & age < 80) {
         points <- 1
       }
-      
+
     } else if(gender == "male" & smoke == TRUE) {
       if(age <20 | age >= 80) {
         points <- NA
@@ -272,17 +274,17 @@ fhrisk_10y <- function(gender, age, cholesterol, smoke, hdl, sysbp, bp_treated, 
     }
     return(points)
   }
-  
-  
-  
+
+
+
   # hdl ---------------------------------------------------------------------
-  
+
   fhrisk_hdl <- function(hdl) {
     points <- 0
     if(hdl >= 60) {
       points <- -1
     } else if(hdl >= 50 & hdl <60) {
-      points <- 0 
+      points <- 0
     } else if(hdl >= 40 & hdl <50) {
       points <- 1
     } else if(hdl < 40) {
@@ -290,11 +292,11 @@ fhrisk_10y <- function(gender, age, cholesterol, smoke, hdl, sysbp, bp_treated, 
     }
     return(points)
   }
-  
-  
-  
+
+
+
   # sys_bp ------------------------------------------------------------------
-  
+
   fhrisk_sbp <- function(gender, sysbp, bp_treated) {
     points <- 0
     if(gender == "female") {
@@ -308,7 +310,7 @@ fhrisk_10y <- function(gender, age, cholesterol, smoke, hdl, sysbp, bp_treated, 
         } else if(sysbp > 160) {
           points <- 4
         }
-        
+
       } else if(bp_treated == TRUE) {
         if(sysbp >= 120 & sysbp <130) {
           points <- 3
@@ -320,7 +322,7 @@ fhrisk_10y <- function(gender, age, cholesterol, smoke, hdl, sysbp, bp_treated, 
           points <- 6
         }
       }
-    } 
+    }
     if(gender == "male") {
       if(bp_treated == FALSE) {
         if(sysbp >= 120 & sysbp <130) {
@@ -332,7 +334,7 @@ fhrisk_10y <- function(gender, age, cholesterol, smoke, hdl, sysbp, bp_treated, 
         } else if(sysbp > 160) {
           points <- 2
         }
-        
+
       } else if(bp_treated == TRUE) {
         if(sysbp >= 120 & sysbp <130) {
           points <- 1
@@ -347,23 +349,23 @@ fhrisk_10y <- function(gender, age, cholesterol, smoke, hdl, sysbp, bp_treated, 
     }
     return(points)
   }
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
   risk <- ""
   age_points <- fhrisk_age(gender = gender, age = age)
-  
+
   chol_points <- fhrisk_chol(gender = gender, age = age, cholesterol = cholesterol)
-  
+
   hdl_points <- fhrisk_hdl(hdl)
-  
+
   sbp_points <- fhrisk_sbp(gender = gender, sysbp = sysbp, bp_treated = bp_treated)
-  
+
   tot_points <- age_points + chol_points + hdl_points + sbp_points
-  
+
   if(is.na(tot_points)) {
   risk <- "Not valiadated for age >= 80!"
   } else if(gender == "female") {
@@ -374,30 +376,30 @@ fhrisk_10y <- function(gender, age, cholesterol, smoke, hdl, sysbp, bp_treated, 
     } else if(tot_points >= 13 & tot_points <=14) {
       risk <- "2%"
     } else if(tot_points == 15) {
-      risk <- "3%" 
+      risk <- "3%"
     } else if(tot_points == 16) {
-      risk <- "4%" 
+      risk <- "4%"
     } else if(tot_points == 17) {
-      risk <- "5%" 
+      risk <- "5%"
     } else if(tot_points == 18) {
-      risk <- "6%" 
+      risk <- "6%"
     } else if(tot_points == 19) {
-      risk <- "8%" 
+      risk <- "8%"
     } else if(tot_points == 20) {
-      risk <- "11%" 
+      risk <- "11%"
     } else if(tot_points == 21) {
-      risk <- "14%" 
+      risk <- "14%"
     } else if(tot_points == 22) {
-      risk <- "17%" 
+      risk <- "17%"
     } else if(tot_points == 23) {
-      risk <- "22%" 
+      risk <- "22%"
     } else if(tot_points == 24) {
-      risk <- "27%" 
+      risk <- "27%"
     } else if(tot_points >= 25) {
-      risk <- ">30%" 
-    } 
+      risk <- ">30%"
+    }
   }
-  
+
   else if(gender == "male") {
     if (tot_points == 0) {
       risk <- "<1%"
@@ -406,30 +408,30 @@ fhrisk_10y <- function(gender, age, cholesterol, smoke, hdl, sysbp, bp_treated, 
     } else if(tot_points >= 5 & tot_points <=6) {
       risk <- "2%"
     } else if(tot_points == 7) {
-      risk <- "3%" 
+      risk <- "3%"
     } else if(tot_points == 8) {
-      risk <- "4%" 
+      risk <- "4%"
     } else if(tot_points == 9) {
-      risk <- "5%" 
+      risk <- "5%"
     } else if(tot_points == 10) {
-      risk <- "6%" 
+      risk <- "6%"
     } else if(tot_points == 11) {
-      risk <- "8%" 
+      risk <- "8%"
     } else if(tot_points == 12) {
-      risk <- "10%" 
+      risk <- "10%"
     } else if(tot_points == 13) {
-      risk <- "12%" 
+      risk <- "12%"
     } else if(tot_points == 14) {
-      risk <- "16%" 
+      risk <- "16%"
     } else if(tot_points == 15) {
-      risk <- "20%" 
+      risk <- "20%"
     } else if(tot_points == 16) {
-      risk <- "25%" 
+      risk <- "25%"
     } else if(tot_points >= 17) {
-      risk <- ">30%" 
+      risk <- ">30%"
     }
   }
-  
+
   return(risk)
 }
 
